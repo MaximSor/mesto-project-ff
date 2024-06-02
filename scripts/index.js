bc59bbd8f1d711cd5b4e27bef48db0fb4783ffb0
Maxim Sorokin <max.sorikin1999@gmail.com>
2024-06-02 18:24:13 +0300

    Финал

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+// @todo: Темплейт карточки+
 
-// @todo: DOM узлы
+// @todo: DOM узлы+
 
-// @todo: Функция создания карточки
+// @todo: Функция создания карточки+
 
-// @todo: Функция удаления карточки
+// @todo: Функция удаления карточки+
 
-// @todo: Вывести карточки на страницу
+// @todo: Вывести карточки на страницу+
+
+const cardTemplate = document.querySelector("#card-template").content;
+
+const cardList = document.querySelector(".places__list");
+
+function creatingCard(card, deleteCard) {
+  const newCard = cardTemplate.cloneNode(true);
+  const cardImage = newCard.querySelector(".card__image");
+  const cardTitle = newCard.querySelector(".card__title");
+  cardTitle.textContent = card.name;
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  const cardDeleteButton = newCard.querySelector(".card__delete-button");
+  cardDeleteButton.addEventListener("click", deleteCard);
+
+  return newCard;
+}
+
+function deleteCard(card) {
+  const listItem = card.target.closest(".card");
+  listItem.remove();
+}
+
+initialCards.forEach((card) => {
+  cardList.append(creatingCard(card, deleteCard));
+});
